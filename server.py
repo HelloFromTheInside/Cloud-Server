0dfe7e9e102eff618a82d2ce1a5e5df32aa72b62
HelloFromTheInside <samuel.backof@stud.th-deg.de>
2024-01-03 23:12:03 +0100

    Added threading

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import socket
+import threading
 
 def create_directory(dir_name): os.makedirs(dir_name, exist_ok=True)
 
@@ -80,7 +81,8 @@ def start_server():
     while True:
         connection, address = server_socket.accept()
         print(f"Connection from {address}")
-        handle_client_commands(connection, address)
+        thread = threading.Thread(target=handle_client_commands, args=(connection, address))
+        thread.start()
 
 if __name__ == "__main__":
     start_server()
