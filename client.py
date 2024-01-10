ecaf861f389533cda060a328d56bf7239829d046
Gepardgame <thomas.degg@gmail.com>
2024-01-10 17:16:20 +0100

    Added Encryption and Decryption

--- client.py
+++ client.py
@@ -1,9 +1,46 @@
 import socket
 
+# For File Encryption
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+from secret import key
+import os
+
+SALT_SIZE = 12
+
+
+def encryption(file_path: str) -> None:
+    cipher = ChaCha20Poly1305(key)
+    plaintext = ""
+    with open(file_path, "rb") as file:
+        plaintext = file.read()
+    salt = os.urandom(SALT_SIZE)
+    print(salt)
+    ciphertext = cipher.encrypt(salt, plaintext, None)
+    print(ciphertext)
+    with open(file_path + ".enc", "wb") as encrypted_file:
+        encrypted_file.write(salt + ciphertext)
+
+
+def decryption(file_path: str) -> None:
+    cipher = ChaCha20Poly1305(key)
+    ciphertext = ""
+    with open(file_path + ".enc", "rb") as encrypted_file:
+        data = encrypted_file.read()
+        salt = data[:SALT_SIZE]
+        ciphertext = data[SALT_SIZE:]
+    print(salt)
+    print(ciphertext)
+    plaintext = cipher.decrypt(salt, ciphertext, None)
+
+    with open(file_path, "wb") as file:
+        file.write(plaintext)
+
+
 def send_command(command):
     client_socket.send(command.encode())
     response = client_socket.recv(1024).decode()
     print(response)
+
 
 # ls & cd only for navigation
 def display_help():
@@ -14,8 +51,9 @@ def display_help():
     print("qp - Ends the connection to the server")
     print("-help - Shows this help")
 
+
 if __name__ == "__main__":
-    host = '127.0.0.1'
+    host = "127.0.0.1"
     port = 12345
 
     try:
@@ -23,12 +61,22 @@ if __name__ == "__main__":
         client_socket.connect((host, port))
         while True:
             command = input(">>> ")
-            if command == '-help':
+            cmd_parts = command.split(" ", 1)
+
+            if command == "-help":
                 display_help()
-            else:
+                continue
+            elif cmd_parts[0] == "uf":
+                file_name = "Files/" + (cmd_parts[1] if len(cmd_parts) > 1 else "")
+                encryption(file_name)
+            elif cmd_parts[0] == "df":
                 send_command(command)
-            if command == 'qp':
+                file_name = "Files/" + (cmd_parts[1] if len(cmd_parts) > 1 else "")
+                decryption(file_name)
+                continue
+            elif command == "qp":
                 break
+            send_command(command)
     except socket.timeout as e:
         print(f"Error connecting to the server: {e}")
         exit()
--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 import os
 import shutil
 import asyncio
-from opaque import CreateCredentialResponse, UserAuth, Ids
 
-def create_directory(dir_name): os.makedirs(dir_name, exist_ok=True)
+# from opaque import CreateCredentialResponse, UserAuth, Ids
+
+
+def create_directory(dir_name):
+    os.makedirs(dir_name, exist_ok=True)
+
 
 async def handle_client_commands(semaphore, reader, writer):
     async with semaphore:
-        address = writer.get_extra_info('peername')
+        address = writer.get_extra_info("peername")
         print(f"Connection from {address}")
         base_dir = os.getcwd()
         create_directory(os.path.join(base_dir, "Files"))
@@ -16,63 +20,73 @@ async def handle_client_commands(semaphore, reader, writer):
 
         while True:
             command = (await reader.read(100)).decode()
-            cmd_parts = command.split(' ', 1)
+            cmd_parts = command.split(" ", 1)
             cmd = cmd_parts[0]
 
             # will be deleted later
-            if cmd == 'ls':
+            if cmd == "ls":
                 try:
                     files = os.listdir(current_dir)
-                    response = '\n'.join(files) if files else "No files found."
+                    response = "\n".join(files) if files else "No files found."
                 except Exception as e:
                     response = f"Error: {e}"
 
             # will be deleted later
             # elif cmd == 'cd':
-                # new_dir = os.path.join(base_dir, cmd_parts[1]) if len(cmd_parts) > 1 else base_dir
-                # if os.path.exists(new_dir) and new_dir in [os.path.join(base_dir, "Files"), os.path.join(base_dir, "Uploads")]:
-                    # current_dir = new_dir
-                    # response = f"Current directory is {os.path.basename(current_dir)}"
-                # else:
-                    # response = "No such directory."
+            # new_dir = os.path.join(base_dir, cmd_parts[1]) if len(cmd_parts) > 1 else base_dir
+            # if os.path.exists(new_dir) and new_dir in [os.path.join(base_dir, "Files"), os.path.join(base_dir, "Uploads")]:
+            # current_dir = new_dir
+            # response = f"Current directory is {os.path.basename(current_dir)}"
+            # else:
+            # response = "No such directory."
 
-            elif cmd == 'uf': # uploading / copying the filename from 'Files' to 'Uploads'
-                file_name = cmd_parts[1] if len(cmd_parts) > 1 else ""
+            elif (
+                cmd == "uf"
+            ):  # uploading / copying the filename from 'Files' to 'Uploads'
+                file_name = (cmd_parts[1] if len(cmd_parts) > 1 else "") + ".enc"
                 source_path = os.path.join(base_dir, "Files", file_name)
                 destination_path = os.path.join(base_dir, "Uploads", file_name)
                 if os.path.exists(source_path):
                     shutil.copy(source_path, destination_path)
                     response = f"File {file_name} was uploaded to 'Uploads'."
-                    print(f"Client {address} uploaded {file_name} to 'Uploads'") # message to the server
+                    print(
+                        f"Client {address} uploaded {file_name} to 'Uploads'"
+                    )  # message to the server
                 else:
                     response = f"File {file_name} not found in 'Files'"
 
-            elif cmd == 'df':  # download / copy file from 'Uploads' to 'Files'
-                file_name = cmd_parts[1] if len(cmd_parts) > 1 else "" # checks if there's additional text after 'df'
+            elif cmd == "df":  # download / copy file from 'Uploads' to 'Files'
+                file_name = (
+                    cmd_parts[1] if len(cmd_parts) > 1 else ""
+                ) + ".enc"  # checks if there's additional text after 'df'
                 source_path = os.path.join(base_dir, "Uploads", file_name)
                 destination_path = os.path.join(base_dir, "Files", file_name)
                 if os.path.exists(source_path):
                     shutil.copy(source_path, destination_path)
                     response = f"File {file_name} was downloaded to 'Files'."
-                    print(f"Client {address} downloaded {file_name} ")  # message to server
+                    print(
+                        f"Client {address} downloaded {file_name} "
+                    )  # message to server
                 else:
                     response = f"File {file_name} not found in 'Uploads'"
 
-            elif cmd == 'rm': # deleting files ether in Files or Uploads
+            elif cmd == "rm":  # deleting files ether in Files or Uploads
                 file_name = cmd_parts[1] if len(cmd_parts) > 1 else ""
                 file_path = os.path.join(current_dir, file_name)
                 if os.path.exists(file_path):
                     os.remove(file_path)
                     response = f"File {file_name} deleted"
-                    print(f"Client {address} deleted {file_name} in 'Files'") # message to server
+                    print(
+                        f"Client {address} deleted {file_name} in 'Files'"
+                    )  # message to server
                 else:
                     response = "File not found"
 
-            elif cmd == 'qp':
+            elif cmd == "qp":
                 response = "Disconnecting."
                 writer.write(response.encode())
                 await writer.drain()
-                print(f"Connection from {address} disconnected") # message to server
+                print(f"Connection from {address} disconnected")  # message to server
                 break
 
             else:
@@ -82,27 +96,27 @@ async def handle_client_commands(semaphore, reader, writer):
             try:
                 await writer.drain()
             except ConnectionResetError:
-                print(f"Connection from {address} disconnected") # message to server
+                print(f"Connection from {address} disconnected")  # message to server
                 break
 
     writer.close()
 
-async def start_server():
 
+async def start_server():
     max_connections = 5
     semaphore = asyncio.Semaphore(max_connections)
 
     server = await asyncio.start_server(
-        lambda r, w: handle_client_commands(semaphore, r, w),
-        '127.0.0.1', 12345
+        lambda r, w: handle_client_commands(semaphore, r, w), "127.0.0.1", 12345
     )
 
     addr = server.sockets[0].getsockname()
-    print(f'Serving on {addr} with a maximum of {max_connections} concurrent connections')
+    print(
+        f"Serving on {addr} with a maximum of {max_connections} concurrent connections"
+    )
 
     async with server:
         await server.serve_forever()
-
 
     # host = '127.0.0.1'
     # port = 12345
@@ -127,6 +141,8 @@ async def start_server():
     #     print("Server stopped")
     #     exit()
     #
+
+
 if __name__ == "__main__":
     try:
         asyncio.run(start_server())
