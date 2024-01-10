b0810e08e911038a83c8a89abc6fe109e5914e4a
Gepardgame <thomas.degg@gmail.com>
2024-01-10 18:58:35 +0100

    en/decryption finished

--- client.py
+++ client.py
@@ -2,41 +2,62 @@ import socket
 
 # For File Encryption
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+from cryptography.exceptions import InvalidTag
 from secret import key
 import os
 
 SALT_SIZE = 12
 
 
-def encryption(file_path: str) -> None:
+def readfile(file_path: str):
+    try:
+        with open(file_path, "rb") as file:
+            text = file.read()
+    except FileNotFoundError:
+        print("The file does not exist.")
+        return 1
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 1
+    return text
+
+
+def write_file(file_path: str, text: bytes) -> int:
+    try:
+        with open(file_path, "wb") as file:
+            file.write(text)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 1
+    return 0
+
+
+def cryption(file_path: str, encrypt: bool) -> int:
     cipher = ChaCha20Poly1305(key)
-    plaintext = ""
-    with open(file_path, "rb") as file:
-        plaintext = file.read()
+    if (text := readfile(file_path + ".enc" * (not encrypt))) == 1:
+        return 1
+    if (text := (encryption if encrypt else decryption)(cipher, text)) == 1:
+        return 2
+    return write_file(file_path + ".enc" * encrypt, text)
+
+
+def encryption(cipher: ChaCha20Poly1305, plaintext: bytes) -> bytes:
     salt = os.urandom(SALT_SIZE)
-    print(salt)
     ciphertext = cipher.encrypt(salt, plaintext, None)
-    print(ciphertext)
-    with open(file_path + ".enc", "wb") as encrypted_file:
-        encrypted_file.write(salt + ciphertext)
+    return salt + ciphertext
 
 
-def decryption(file_path: str) -> None:
-    cipher = ChaCha20Poly1305(key)
-    ciphertext = ""
-    with open(file_path + ".enc", "rb") as encrypted_file:
-        data = encrypted_file.read()
-        salt = data[:SALT_SIZE]
-        ciphertext = data[SALT_SIZE:]
-    print(salt)
-    print(ciphertext)
-    plaintext = cipher.decrypt(salt, ciphertext, None)
-
-    with open(file_path, "wb") as file:
-        file.write(plaintext)
+def decryption(cipher: ChaCha20Poly1305, data: bytes):
+    salt = data[:SALT_SIZE]
+    ciphertext = data[SALT_SIZE:]
+    try:
+        plaintext = cipher.decrypt(salt, ciphertext, None)
+    except InvalidTag:
+        return 1
+    return plaintext
 
 
-def send_command(command):
+def send_command(command) -> None:
     client_socket.send(command.encode())
     response = client_socket.recv(1024).decode()
     print(response)
@@ -68,18 +89,19 @@ if __name__ == "__main__":
                 continue
             elif cmd_parts[0] == "uf":
                 file_name = "Files/" + (cmd_parts[1] if len(cmd_parts) > 1 else "")
-                encryption(file_name)
+                if cryption(file_name, True):
+                    continue
             elif cmd_parts[0] == "df":
                 send_command(command)
                 file_name = "Files/" + (cmd_parts[1] if len(cmd_parts) > 1 else "")
-                decryption(file_name)
+                if cryption(file_name, False):
+                    print("File is corrupted, please continue with precaution!")
                 continue
-            elif command == "qp":
-                break
             send_command(command)
+            if command == "qp":
+                break
     except socket.timeout as e:
         print(f"Error connecting to the server: {e}")
-        exit()
     finally:
         client_socket.close()
         print("Connection closed")
